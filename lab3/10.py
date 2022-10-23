38c4109b64a94051571767cdc0cd32effe2693f0
mark Samuel <marksamfd511@gmail.com>
2022-10-23 20:50:36 +0200

    d

--- lab3/10.py
+++ lab3/10.py
@@ -13,4 +13,23 @@ sum = 0
 for grade in validList:
     sum+=int(grade)
 
-print(valid, validList01,sum/len(validList))
+
+lowestGrade = 0
+lowestGradeIndex = 0
+highestGrade = 0
+highestGradeIndex = 0
+
+counterGrade = 0
+
+for stgrade in validList:
+    if int(stgrade) > highestGrade:
+        highestGrade = int(stgrade)
+        highestGradeIndex = counterGrade
+    elif (int(stgrade)<lowestGrade or lowestGrade == 0)and int(stgrade) !=0:
+        lowestGrade = int(stgrade)
+        lowestGradeIndex = counterGrade
+    counterGrade+=1
+
+print("the valid matrix is:",valid, "\nthe valid  is:",validList01,"\nthe avareage  is:",sum/len(validList),"\nthe highest grade is:",highestGrade,
+      "\nit's index is:", highestGradeIndex,"\nthe valid lowest grade is:",lowestGrade,"\nit's index is:", lowestGradeIndex,)
+#90 10 50 2 -2 1
