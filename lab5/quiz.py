a0b0884d93fa16826cbbeb5d1e49e613e9df4c23
mark Samuel <marksamfd511@gmail.com>
2022-11-06 11:38:34 +0200

    added quiz

---
+++ lab5/quiz.py
@@ -0,0 +1,5 @@
+componant = input("What did the team members bring? : ")
+if "battery" in componant:
+    print("I don't need to bring a battery")
+else:
+    print("I will need to bring a battery")
